93bdb93cd15f8d49f6784cf05f69c36dc666d56e
Matheus <luzmatheusrodrigues@gmail.com>
2021-11-19 19:35:48 -0300

    movimentos da cobra

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ let box = 32;
 let snake = [];
 snake[0] = {
     x: 8 * box,
-    y: 8 * box 
+    y: 8 * box
 }
+let direction = "right";
 
 
 function criarBG() {
@@ -14,11 +15,33 @@ function criarBG() {
 }
 
 function criarCobra() {
-    for(i=0; i < snake.length; i++){
+    for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+function iniciarJogo(){
+    criarBG();
+    criarCobra();
 
-criarBG();
-criarCobra();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
